8375a051fb7336a93a750400da207c5f72b2877f
parth-soni07 <202151104@iiitvadodara.ac.in>
2024-05-26 20:22:37 +0530

    added businnes wallet changes

--- contracts/Cresento.sol
+++ contracts/Cresento.sol
@@ -1,50 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC720.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract CresentoWallet is ERC721URIStorage, Ownable{
-    string public username;
-    address public userAddress;
-    uint64 public tokenAmount;
-    address payable recepientAddress;
-    bool public signedIn =  false;
-    
-    event TransferSuccessful(address payable recepientAddress, uint64 tokenAmount);
 
-    mapping (string => bool) wallet;
-    // This is the function to mint the tokens, the minted tokens will be then used to be transferred
-    function mintToken (address _to, uint64 _tokenAmount, string calldata _uri) external onlyOwner {
-        _mint(_to,_tokenAmount);
-        _setTokenURI(_tokenAmount, _uri);
+contract CresentoCrossChainWallet {
+    mapping (string => mapping (string => bool) ) txnPairs;
+    mapping (string => uint256) balances;
+
+    event Transfer(string indexed username, string indexed receiver, uint64 amount);
+
+    constructor() {
+        
     }
-    // Create Wallet Button will be used to create an account using the username & entered password.
-    function createWallet(string memory username, string memory password) public {
-        require(!wallet[username], "Username Exists");
-        wallet[username] = true;
+    function addTxnPairs(string memory user1, string memory user2) public {
+        txnPairs[user1][user2] = true;
+        txnPairs[user2][user1] = true;
     }
-    // for login and stuff, I dont think the blockchain will be responsible to take care of, js will handle it, only the username will be sent to the backend
-    constructor (string memory _username , address _userAddress, uint64 _tokenAmount, address payable _recepientAddress) ERC721 ("CresentoToken", "CTEA") Ownable (msg.sender){
-        require(bytes(_username).length > 0, "Username cannot be empty");
-        require(_recepientAddress != address(0), "Recipient address cannot be empty");
-        require(_userAddress != address(0), "Recipient address cannot be empty");
-        require(tokenAmount > 0, "You cannot send 0 to Recepient");
-        username = _username;
-        userAddress = _userAddress;
-        tokenAmount = _tokenAmount;
-        recepientAddress = _recepientAddress;
+
+    function transfer(string memory un, string memory rec, uint64 amt, string memory authorizedUser) public {
+        require(txnPairs[un][authorizedUser], "Unauthorized Transfer");
+        require(balances[un] >= amt, "Insuffecient Balance");
+
+        balances[un] -= amt;
+        balances[rec] += amt;
+        
     }
-    // Transfer will be responsible for the transfer of the money
-    function transfer() public payable {
-        require(msg.value >= tokenAmount, "Insuffecient token to complete the transaction");
-        // ERC721 token = ERC721()
-    }
-    // basically the logic behind these contracts is simple, you just have one main function and all the functions call 
-    // themselves in a chainical order so that all the work is done, so the function that is called sabse pehele is constructor
-    // 
-    function transferToken () public payable {
-        require(msg.value >= tokenAmount, "Insuffecient token to complete the transaction");
-        recepientAddress.transfer(tokenAmount);
-        emit TransferSuccessful(recepientAddress, tokenAmount);
-    }
+
 }
